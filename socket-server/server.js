fe742c9455658df693d383c9d5100b8ca0fbabd4
Saikat Bera <berasaikat729@gmail.com>
2025-03-30 14:21:43 +0530

    Update server.js

--- socket-server/server.js
+++ socket-server/server.js
@@ -6,15 +6,15 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://hackathon-2xo7.vercel.app/',
+  origin: process.env.FRONTEND_URL || 'https://ai-mock-interview-ten-omega.vercel.app/',
   methods: ['GET', 'POST'],
   credentials: true
 }));
-
+//https://hackathon-2xo7.vercel.app/
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'https://hackathon-2xo7.vercel.app/',
+    origin: process.env.FRONTEND_URL || 'https://ai-mock-interview-ten-omega.vercel.app/',
     methods: ['GET', 'POST'],
     credentials: true
   },
